06690ab04af1698be4ec2d11ec9157679f925bd8
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-06 21:36:29 -0300

    feat: imagens e texto para perfil pessoal

---
+++ react-project/src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+
+import paisagem from '../../assets/paisagem.jpg'
+
+export default function Main() {
+    return(
+        <main>
+            <div>
+                <img src={paisagem} alt="Paisagem" />
+                <div>
+                    <img src="" alt="" />
+                    <h2>Gabriel Trindade</h2>
+                    <p>Dev front-end</p>
+                </div>
+            </div>
+        </main>
+    )
+}
